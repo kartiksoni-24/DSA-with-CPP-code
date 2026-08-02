7cca30650d1437c4d450e9a4914890ffea735799
Kartik soni <sonikartik2444@gmail.com>
2026-08-02 15:48:27 +0530

    Add Tiling Problem solution using Recursion

---
+++ Recurrsion/10_Tiling_Problem.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int tilingProblem(int n){
+    if(n == 0 || n == 1){
+        return 1;
+    }
+
+    return tilingProblem(n-1) + tilingProblem(n-2);
+}
+
+int main(){
+    cout << tilingProblem(5) << endl;
+    return 0;
+}
