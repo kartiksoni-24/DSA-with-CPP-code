54174bfbadbdb46a6b89d2ab26eac44670a3dfe5
Kartik soni <sonikartik2444@gmail.com>
2026-08-20 19:36:55 +0530

    implement stack using Deque

---
+++ Queue/10_Stack_Using_Deque.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+class Stack{
+    deque<int> deq;
+    public:
+    void push(int val){
+        deq.push_back(val);
+    }
+
+    void pop(){
+        deq.pop_back();
+    }
+
+    int top(){
+        return deq.back();
+    }
+
+    bool empty(){
+        return deq.empty();
+    }
+};
+
+int main(){
+    Stack s;
+
+    for (int i = 0; i < 10; i++)
+    {
+        s.push(i);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    
+    return 0;
+}
