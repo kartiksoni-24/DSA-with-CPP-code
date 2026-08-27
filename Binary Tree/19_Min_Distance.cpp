cd9bc39607c729dcc132c99992e42fc1a32e5af9
Kartik soni <sonikartik2444@gmail.com>
2026-08-27 23:32:24 +0530

    solved problems of binary tree

--- Binary Tree/19_Min_Distance.cpp
+++ Binary Tree/19_Min_Distance.cpp
@@ -33,12 +33,12 @@ Node *createTree(vector<int> &nodes)
     return currNode;
 }
 
-Node* LCA(Node* root, int n1, int n2){ // O(n), O(1)
+Node* LCA(Node* root, int n1, int n2){
     if(root == NULL){
         return NULL;
     }
 
-    if(root ->data == n1 || root->data == n2){
+    if(root->data == n1 || root->data == n2){
         return root;
     }
 
@@ -52,11 +52,42 @@ Node* LCA(Node* root, int n1, int n2){ // O(n), O(1)
     return left == NULL ? right : left;
 }
 
+int dist(Node* root, int n){
+    if(root == NULL){
+        return -1;
+    }
+
+    if(root->data == n){
+        return 0;
+    }
+
+    int leftDist = dist(root->left, n);
+    if(leftDist != -1){
+        return leftDist + 1;
+    }
+
+    int rightDist = dist(root->right, n);
+    if(rightDist != -1){
+        return rightDist + 1;
+    }
+
+    return -1;
+}
+
+int minDist(Node* root, int n1, int n2){ // O(n)
+    Node* lca = LCA(root, n1, n2);
+
+    int distLeft = dist(lca, n1);
+    int distRight = dist(lca, n2);
+
+    return distLeft + distRight;
+}
+
 int main()
 {
     vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
     Node *root = createTree(nodes);
 
-    cout << LCA(root, 4, 5)->data;
+    cout << minDist(root, 5, 1);
     return 0;
 }
