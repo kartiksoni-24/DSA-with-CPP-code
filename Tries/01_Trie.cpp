9af50ef5f4dabe1589f693481f54a7a40e44d940
Kartik soni <sonikartik2444@gmail.com>
2026-09-21 17:52:42 +0530

    learn insert in trie

--- Tries/01_Trie.cpp
+++ Tries/01_Trie.cpp
@@ -20,9 +20,27 @@ public:
     Trie(){
         root = new Node();
     }
+
+    void insert(string key){ // O(n*l)
+        Node* temp = root;
+
+        for(int i = 0; i < key.size(); i++){
+            if(temp->children.count(key[i]) == 0){
+                temp->children[key[i]] = new Node();
+            }
+            temp = temp->children[key[i]];
+        }
+        temp->isEnd = false;
+    }
 };
 
 int main(){
+    vector<string> words = {"the", "a", "there", "their", "thee", "any"};
     Trie trie;
+
+    for(int i = 0; i<words.size(); i++){
+        trie.insert(words[i]);
+    }
+
     return 0;
 }
